d8cf22a1c4e907947d54b7754f818c302bca7d80
thebenrub <thebenrub@gmail.com>
2026-06-29 12:21:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function getInput(){
+    let userInput = prompt("insert a number: ");
+    for(let i = 0; i<=userInput; i++){
+        if(i % 2 ==0){
+            document.writeln("<div class =header>"+ i+ "</div>")
+        }
+        x = Math.sqrt();
+    }
+    alert("Done !");
+
+}
+
+
+
+
+
+getInput();
