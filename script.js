14e7f3c7a7e0d42f058ed15db6c7a20a57a0ea1e
thebenrub <thebenrub@gmail.com>
2026-06-30 10:46:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function createBoard() {
     }
 }
 createBoard();
+
+function startGame() {
+
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(timerId);
+}
+
+
+function move() {
+
+    const hitBottom = (currentSnake[0] + 20 >= 400 && direction === 20);
+    const hitTop = (currentSnake - 20 < 0 && direction === -20);
+    const hitRight = (currentSnake[0] % 20 === 19 && direction === 1);
+    const hitLeft = (currentSnake[0] % 20 === 0 && direction == -1);
+    const hitSelf = squares[currentSnake[0] + direction]?.classList.contains('snake');
+}
